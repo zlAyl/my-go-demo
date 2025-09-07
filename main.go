5eeb91fb36b7f638d73d0b0b4a48298738dff211
lee <zlplay2014@163.com>
2025-09-07 11:08:41 +0800

    go基础作业1

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/zlAyl/my-go-demo/task1"
+)
+
+func main() {
+	//1.只出现一次的数字
+	nums := []int{1, 1, 2, 3, 3, 6, 2, 5, 5, 6, 7}
+	result := task1.SingleNumber(nums)
+	if result == nil {
+		fmt.Println("没有只出现一次的数字")
+	} else {
+		fmt.Println("只出现一次的数字有:", result)
+	}
+
+	//2.回文数
+	x := -1221
+	result2 := task1.IsPalindrome(x)
+	if result2 == false {
+		fmt.Printf("数字 x = %d 不是回文数\n", x)
+	} else {
+		fmt.Printf("数字 x = %d 是回文数\n", x)
+	}
+
+	//3.有效的括号
+	str := "([])[]{"
+	result3 := task1.IsValid(str)
+	if result3 == false {
+		fmt.Printf("字符串 str = %s 不是有效的括号\n", str)
+	} else {
+		fmt.Printf("字符串 str = %s 是有效的括号\n", str)
+	}
+
+	//4.最长公共
+	strs := []string{"flower", "flow"}
+	result4 := task1.LongestCommonPrefix(strs)
+	fmt.Printf("strs 最长公共前缀为 %s \n", result4)
+
+	//5.加一
+	digits := []int{1, 2, 9}
+	result5 := task1.PlusOne(digits)
+	if result5 == nil {
+		fmt.Println("digits 加一后 处理错误 ")
+	} else {
+		fmt.Println("digits 加一后的值为 ", result5)
+	}
+
+	//6.删除有序数组中的重复项
+	nums = []int{1, 1, 2, 3, 4, 4, 5, 6, 7, 7}
+	lens, result6 := task1.RemoveDuplicates(nums)
+	fmt.Println("删除有序数组中的重复项后数组的长度：", lens)
+	fmt.Println("删除有序数组中的重复项后的数组为:", result6)
+
+	//7.合并区间
+	intervals := [][]int{{1, 3}, {2, 6}, {3, 7}, {8, 9}}
+	result7 := task1.Merge(intervals)
+	fmt.Println("合并后为:", result7)
+
+	//8.两数之和
+	nums = []int{2, 7, 11, 15}
+	target := 17
+	result8 := task1.TwoSum(nums, target)
+	fmt.Println("两数之和得到目标值得两个数为:", result8)
+}
---
+++ task1/task1.go
@@ -0,0 +1,186 @@
+package task1
+
+import (
+	"sort"
+	"strconv"
+)
+
+func SingleNumber(nums []int) []int {
+	numberSet := make(map[int]int)
+	for _, num := range nums {
+		numberSet[num] += 1
+	}
+	var result []int
+	for k, v := range numberSet {
+		if v == 1 {
+			result = append(result, k)
+		}
+	}
+	return result
+}
+
+// IsPalindrome 判断数字是否是回文数
+func IsPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	iToStr := strconv.Itoa(x)
+	var newX string
+	for k, _ := range iToStr {
+		newX += string(iToStr[len(iToStr)-k-1])
+	}
+
+	if newX == iToStr {
+		return true
+	}
+	return false
+}
+
+// IsValid 判断是否是有效的括号
+func IsValid(s string) bool {
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	strMap := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+	stack := make([]rune, 0)
+	for _, v := range s {
+		right, isRight := strMap[v]
+		if isRight == true { //表明v在key值里面 是右括号
+			//检查栈是否为空或栈顶元素不匹配
+			if len(stack) == 0 || stack[len(stack)-1] != right {
+				return false
+			}
+			// 弹出匹配的左括号
+			stack = stack[:len(stack)-1]
+		} else { //表明v不在key值里面 是左括号 左括号压入栈里
+			stack = append(stack, v)
+		}
+	}
+	return len(stack) == 0
+}
+
+// LongestCommonPrefix 最长公共前缀
+func LongestCommonPrefix(strs []string) string {
+
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+
+		j := 0
+		for j < len(prefix) && j < len(strs[i]) && prefix[j] == strs[i][j] {
+			j++
+		}
+
+		prefix = prefix[:j]
+
+		if prefix == "" {
+			break
+		}
+
+	}
+
+	return prefix
+}
+
+// PlusOne 加一
+func PlusOne(digits []int) []int {
+	n := len(digits)
+	for i := n - 1; i >= 0; i-- {
+		// 当前位加1
+		digits[i]++
+
+		// 如果加1后小于10，没有进位，直接返回
+		if digits[i] < 10 {
+			return digits
+		}
+
+		// 否则，当前位设为0，进位到前一位
+		digits[i] = 0
+	}
+	// 如果所有位都进位了，需要在数组前面添加1
+	return append([]int{1}, digits...)
+
+	//var str string
+	//for _, v := range digits {
+	//	str += strconv.Itoa(v)
+	//}
+	//
+	//newInt, err := strconv.Atoi(str)
+	//if err != nil {
+	//	return nil
+	//}
+	//newInt += 1
+	//
+	//newStr := strconv.Itoa(newInt)
+	//
+	//digits = make([]int, len(newStr))
+	//for i, v := range newStr {
+	//	digit, _ := strconv.Atoi(string(v))
+	//	digits[i] = digit
+	//}
+	//fmt.Println(digits)
+	//return digits
+}
+
+func RemoveDuplicates(nums []int) (int, []int) {
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		// 当发现不重复的元素时
+		if nums[j] != nums[i] {
+			// 将不重复的元素移动到正确位置
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1, nums[:i+1]
+}
+
+func Merge(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return [][]int{}
+	}
+	// 按区间的起始点排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	merged := [][]int{intervals[0]}
+	for i := 1; i < len(intervals); i++ {
+		last := merged[len(merged)-1]
+		current := intervals[i]
+
+		// 如果当前区间与最后一个区间重叠，则合并
+		if current[0] <= last[1] {
+			if current[1] > last[1] {
+				last[1] = current[1]
+			}
+		} else {
+			// 否则，将当前区间添加到结果中
+			merged = append(merged, current)
+		}
+	}
+
+	return merged
+}
+
+func TwoSum(nums []int, target int) []int {
+	var sums []int
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			// 检查两数之和是否等于目标值
+			if nums[i]+nums[j] == target {
+				sums = append(sums, nums[i], nums[j])
+			}
+		}
+	}
+	return sums
+}
